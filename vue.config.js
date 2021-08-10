0b996ee6eb836a3203538fe3e333a2ea7b37b806
Florian Sanders <sanders.florian@gmail.com>
2021-08-10 14:35:26 +0200

    fix: default page title

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
       filename: 'index.html',
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Suivi de commande - Tediber',
+      title: 'Test de recrutement - Suivi de commande - Tediber',
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index'],
